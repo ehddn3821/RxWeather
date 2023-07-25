ba602259fba57f9fd3620d54007cafdc927a248c
dongwoo kang <ehddn3821@naver.com>
2023-07-25 16:29:33 +0900

    chore: .gitignore

---
+++ RxWeather/Source/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  RxWeather
+//
+//  Created by dwKang on 2023/07/25.
+//
+
+import Foundation
+
+struct APIKey {
+    static let key: String = "a208001c687bc0ab7355c8244a06857d"
+}
